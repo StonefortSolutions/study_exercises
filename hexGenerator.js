5627070e5ecb4b11126ad371d7429dc0475467f6
Scott Weaver <scott@weaverscott.com>
2023-06-15 09:34:21 -0500

    hextorgb

---
+++ hexGenerator.js
@@ -0,0 +1,52 @@
+// create two functions, one that takes a hex code and coverts to its RGB value and vis versa
+
+//if you are able to do the above, create an additional function that allows you to take a hexcode and make it more green (not pure green)
+
+const hexToRGB = (hex, red, green, blue) => {
+  let r = parseInt(hex.slice(1, 3), 16);
+  let g = parseInt(hex.slice(3, 5), 16);
+  let b = parseInt(hex.slice(5, 7), 16);
+  if (red && red < 256 && typeof red === "number") {
+    r = r + red > 255 ? 255 : r + red;
+  }
+  if (green && green < 256 && typeof green === "number") {
+    g = g + green > 255 ? 255 : g + green;
+  }
+  if (blue && blue < 256 && typeof blue === "number") {
+    b = b + blue > 255 ? 255 : b + blue;
+  }
+
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+const hexToGreen = (hex, amount) => {
+  return hexToRGB(hex, 0, amount, 0);
+};
+
+const rgbToHex = (rgb) => {
+  let hex = "#";
+  const rgbArr = rgb.slice(4, rgb.length - 1).split(", ");
+  for (const color of rgbArr) {
+    let hexColor = parseInt(color).toString(16);
+    hexColor = hexColor.length === 1 ? "0" + hexColor : hexColor;
+    hex += hexColor;
+  }
+  return hex;
+};
+
+const testHex = "#8a2580";
+const testHex2 = "#FF00FF";
+const testRGB = "rgb(138, 37, 128)";
+const testRGB2 = "rgb(255, 0, 255)";
+
+console.log("=======TEST 1 ========");
+console.log(testHex, "Hex Value");
+console.log(hexToRGB(testHex), "RGB Value");
+console.log(hexToGreen(testHex, 50), "RGB + 50 Green Value");
+console.log(rgbToHex(testRGB), "RGB to Hex");
+
+console.log("=======TEST 2 ========");
+console.log(testHex2, "Hex Value");
+console.log(hexToRGB(testHex2), "RGB Value");
+console.log(hexToGreen(testHex2, 50), "RGB + 50 Green Value");
+console.log(rgbToHex(testRGB2), "RGB to Hex");
