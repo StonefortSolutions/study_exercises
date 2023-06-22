d0ff9f9088eb7dbb871f5e129db224535acbd8c4
Scott Weaver <scott@weaverscott.com>
2023-06-22 10:21:29 -0500

    comments

--- oreo.js
+++ oreo.js
@@ -7,6 +7,9 @@
 //if an odd number of items in the initial array, have one pair that is a trio
 // example output: [1,2,3,4,5] => [[1,3],[2,5,4]]
 
+//O(n) time complexity
+
+
 /**
  * Returns an array of pairs from the given array, randomized, with one trio if the array is odd
  * @param {array} arr
@@ -15,11 +18,16 @@
 function createPairs(arr) {
     const newArray = [];
     const sortedArr = arr.sort(() => Math.random() - 0.5);
+    //for loop to iterate through the array, and push pairs into the new array
+    //increment by 2 to get pairs
     for (let i = 0; i < sortedArr.length; i += 2) {
+        //if the array is odd, push the last item into the last pair
+        //break out of the loop to skip the last push to newArray
         if (i + 1 === sortedArr.length && sortedArr.length % 2 !== 0) {
             newArray[newArray.length - 1].push(sortedArr[i]);
             break;
         }
+        //push pairs into the new array
         newArray.push([sortedArr[i], sortedArr[i + 1]]);
     }
     return newArray;
